d91b90dc0b0119254709b732f14eefe13f7b3b0a
it21336218 <it21336218@my.sliit.lk>
2025-03-13 02:49:51 +0530

    add front end and backend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // If using HTML
+    "./src/**/*.{js,ts,jsx,tsx}", // If using React, Vue, or Svelte
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
